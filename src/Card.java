21ace8631f63f6ffe2e524003f60926e38155b1a
lennardwlk <lennardwilke.2008@gmail.com>
2026-05-13 08:17:03 +0200

    init commit Getter Methoden in Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,13 +1,27 @@
 public class Card {
-    private String rank;
-    private int suit;
+    private String name;
+    private int wert;
+    private char farbe;
 
     public Card(){
 
     }
 
     public Card(String name, int wert, char farbe){
-
+        this.name=name;
+        this.wert=wert;
+        this.farbe=farbe;
     }
 
+    public String getName(){
+        return this.name;
+    }
+
+    public int getWert(){
+        return this.wert;
+    }
+
+    public char getFarbe(){
+        return this.farbe;
+    }
 }

8cefad22b2ff9aa241c9338d3057c5a34e2986d4
lennardwlk <lennardwilke.2008@gmail.com>
2026-05-21 08:05:20 +0200

    init commit kleinigkeiten

--- src/Card.java
+++ src/Card.java
@@ -1,8 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Random;
-
 public class Card {
     private String name;
     private int wert;

ea91ddddeb8132993c89494ac440d1ee95c18ecf
lennardwlk <lennardwilke.2008@gmail.com>
2026-05-22 12:05:14 +0200

    init commit Dealer.java bekommt neue Methoden

--- src/Dealer.java
+++ src/Dealer.java
@@ -2,11 +2,22 @@ public class Dealer {
     private Hand hand;
     private Shoe shoe;
 
+
+    public Dealer(){
+        this.hand=hand;
+        this.shoe=shoe;
+    }
     public void playHand(){
 
     }
     public void start(){
 
+    }
+
+    public void zieheZweiKarten(Shoe shoe){
+        this.hand.karteZiehen(shoe.getObersteKarte());
+        System.out.println("Karten Dealer:" + "\n");
+        //System.out.println(this.hand.getName());
     }
 
 }
